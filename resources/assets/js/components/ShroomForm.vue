a42ead4cd5d9cd87a46c2091427abea58e97d89a
Dario <dnedic@etfos.hr>
2016-07-05 23:39:58 +0200

    Updated option values

--- resources/assets/js/components/ShroomForm.vue
+++ resources/assets/js/components/ShroomForm.vue
@@ -67,10 +67,10 @@
                     <fieldset class="form-group">
                         <label for="gillAttachment">Attachment</label>
                         <select class="form-control" id="gillAttachment" name="gillAttachment">
-                            <option value="f">Fibrous</option>
-                            <option value="g">Grooves</option>
-                            <option value="y">Scaly</option>
-                            <option value="s">Smooth</option>
+                            <option value="a">Attached</option>
+                            <option value="d">Descending</option>
+                            <option value="f">Free</option>
+                            <option value="n">Notched</option>
                         </select>
                     </fieldset>
                 </div>
@@ -80,14 +80,17 @@
                         <label for="gillColor">Color</label>
                         <select class="form-control" id="gillColor" name="gillColor">
                             <option value="n">Brown</option>
+                            <option value="k">Black</option>
                             <option value="b">Buff</option>
-                            <option value="c">Cinnamon</option>
                             <option value="g">Gray</option>
-                            <option value="p">Green</option>
-                            <option value="u">Pink</option>
-                            <option value="e">Purple</option>
+                            <option value="r">Green</option>
+                            <option value="p">Pink</option>
+                            <option value="u">Purple</option>
                             <option value="w">White</option>
-                            <option value="i">Yellow</option>
+                            <option value="y">Yellow</option>
+                            <option value="h">Chocolate</option>
+                            <option value="o">Orange</option>
+                            <option value="e">Red</option>
                         </select>
                     </fieldset>
                 </div>
@@ -96,12 +99,9 @@
                     <fieldset class="form-group">
                         <label for="gillSpacing">Spacing</label>
                         <select class="form-control" id="gillSpacing" name="gillSpacing">
-                            <option value="b">Bell</option>
-                            <option value="c">Conical</option>
-                            <option value="x">Convex</option>
-                            <option value="f">Flat</option>
-                            <option value="k">Knobbed</option>
-                            <option value="s">Sunken</option>
+                            <option value="w">Crowded</option>
+                            <option value="c">Close</option>
+                            <option value="d">Distant</option>
                         </select>
                     </fieldset>
                 </div>
@@ -110,12 +110,8 @@
                     <fieldset class="form-group">
                         <label for="gillSize">Size</label>
                         <select class="form-control" id="gillSize" name="gillSize">
-                            <option value="b">Bell</option>
-                            <option value="c">Conical</option>
-                            <option value="x">Convex</option>
-                            <option value="f">Flat</option>
-                            <option value="k">Knobbed</option>
-                            <option value="s">Sunken</option>
+                            <option value="b">Broad</option>
+                            <option value="n">Narrow</option>
                         </select>
                     </fieldset>
                 </div>
@@ -132,10 +128,8 @@
                     <fieldset class="form-group">
                         <label for="stalkShape">Shape</label>
                         <select class="form-control" id="stalkShape" name="stalkShape">
-                            <option value="f">Fibrous</option>
-                            <option value="g">Grooves</option>
-                            <option value="y">Scaly</option>
-                            <option value="s">Smooth</option>
+                            <option value="t">Tapering</option>
+                            <option value="e">Enlarging</option>
                         </select>
                     </fieldset>
                 </div>
@@ -144,15 +138,13 @@
                     <fieldset class="form-group">
                         <label for="stalkRoot">Root</label>
                         <select class="form-control" id="stalkRoot" name="stalkRoot">
-                            <option value="n">Brown</option>
-                            <option value="b">Buff</option>
-                            <option value="c">Cinnamon</option>
-                            <option value="g">Gray</option>
-                            <option value="p">Green</option>
-                            <option value="u">Pink</option>
-                            <option value="e">Purple</option>
-                            <option value="w">White</option>
-                            <option value="i">Yellow</option>
+                            <option value="">Bulbous</option>
+                            <option value="c">Club</option>
+                            <option value="u">Cup</option>
+                            <option value="e">Equal</option>
+                            <option value="z">Rhizomorphs</option>
+                            <option value="r">Rooted</option>
+                        <!-- Pogledati poslije --> <option value="?">Missing</option>
                         </select>
                     </fieldset>
                 </div>
@@ -161,12 +153,10 @@
                     <fieldset class="form-group">
                         <label for="stalkSurfaceAboveRing">Surface above ring</label>
                         <select class="form-control" id="stalkSurfaceAboveRing" name="stalkSurfaceAboveRing">
-                            <option value="b">Bell</option>
-                            <option value="c">Conical</option>
-                            <option value="x">Convex</option>
-                            <option value="f">Flat</option>
-                            <option value="k">Knobbed</option>
-                            <option value="s">Sunken</option>
+                            <option value="f">Fibrous</option>
+                            <option value="y">Scaly</option>
+                            <option value="k">Silky</option>
+                            <option value="s">Smooth</option>
                         </select>
                     </fieldset>
                 </div>
@@ -175,12 +165,10 @@
                     <fieldset class="form-group">
                         <label for="stalkSurfaceBelowRing">Surface below ring</label>
                         <select class="form-control" id="stalkSurfaceBelowRing" name="stalkSurfaceBelowRing">
-                            <option value="b">Bell</option>
-                            <option value="c">Conical</option>
-                            <option value="x">Convex</option>
-                            <option value="f">Flat</option>
-                            <option value="k">Knobbed</option>
-                            <option value="s">Sunken</option>
+                            <option value="f">Fibrous</option>
+                            <option value="y">Scaly</option>
+                            <option value="k">Silky</option>
+                            <option value="s">Smooth</option>
                         </select>
                     </fieldset>
                 </div>
@@ -189,12 +177,16 @@
                     <fieldset class="form-group">
                         <label for="stalkColorAboveRing">Color above ring</label>
                         <select class="form-control" id="stalkColorAboveRing" name="stalkColorAboveRing">
-                            <option value="b">Bell</option>
-                            <option value="c">Conical</option>
-                            <option value="x">Convex</option>
-                            <option value="f">Flat</option>
-                            <option value="k">Knobbed</option>
-                            <option value="s">Sunken</option>
+                            <option value="n">Brown</option>
+                            <option value="b">Buff</option>
+                            <option value="c">Cinnamon</option>
+                            <option value="g">Gray</option>
+                            <option value="p">Pink</option>
+                            <option value="w">White</option>
+                            <option value="y">Yellow</option>
+                            <option value="h">Chocolate</option>
+                            <option value="o">Orange</option>
+                            <option value="e">Red</option>
                         </select>
                     </fieldset>
                 </div>
@@ -203,12 +195,16 @@
                     <fieldset class="form-group">
                         <label for="stalkColorBelowRing">Color below ring</label>
                         <select class="form-control" id="stalkColorBelowRing" name="stalkColorBelowRing">
-                            <option value="b">Bell</option>
-                            <option value="c">Conical</option>
-                            <option value="x">Convex</option>
-                            <option value="f">Flat</option>
-                            <option value="k">Knobbed</option>
-                            <option value="s">Sunken</option>
+                            <option value="n">Brown</option>
+                            <option value="b">Buff</option>
+                            <option value="c">Cinnamon</option>
+                            <option value="g">Gray</option>
+                            <option value="p">Pink</option>
+                            <option value="w">White</option>
+                            <option value="y">Yellow</option>
+                            <option value="h">Chocolate</option>
+                            <option value="o">Orange</option>
+                            <option value="e">Red</option>
                         </select>
                     </fieldset>
                 </div>
@@ -226,10 +222,8 @@
                         <fieldset class="form-group">
                             <label for="veilType">Type</label>
                             <select class="form-control" id="veilType" name="veilType">
-                                <option value="f">Fibrous</option>
-                                <option value="g">Grooves</option>
-                                <option value="y">Scaly</option>
-                                <option value="s">Smooth</option>
+                                <option value="p">Partial</option>
+                                <option value="u">Universal</option>
                             </select>
                         </fieldset>
                     </div>
@@ -239,14 +233,9 @@
                             <label for="veilColor">Color</label>
                             <select class="form-control" id="veilColor" name="veilColor">
                                 <option value="n">Brown</option>
-                                <option value="b">Buff</option>
-                                <option value="c">Cinnamon</option>
-                                <option value="g">Gray</option>
-                                <option value="p">Green</option>
-                                <option value="u">Pink</option>
-                                <option value="e">Purple</option>
+                                <option value="o">Orange</option>
                                 <option value="w">White</option>
-                                <option value="i">Yellow</option>
+                                <option value="y">Yellow</option>
                             </select>
                         </fieldset>
                     </div>
@@ -260,12 +249,9 @@
                         <fieldset class="form-group">
                             <label for="ringNumber">Number</label>
                             <select class="form-control" id="ringNumber" name="ringNumber">
-                                <option value="b">Bell</option>
-                                <option value="c">Conical</option>
-                                <option value="x">Convex</option>
-                                <option value="f">Flat</option>
-                                <option value="k">Knobbed</option>
-                                <option value="s">Sunken</option>
+                                <option value="n">None</option>
+                                <option value="o">One</option>
+                                <option value="t">Two</option>
                             </select>
                         </fieldset>
                     </div>
@@ -274,12 +260,14 @@
                         <fieldset class="form-group">
                             <label for="ringType">Type</label>
                             <select class="form-control" id="ringType" name="ringType">
-                                <option value="b">Bell</option>
-                                <option value="c">Conical</option>
-                                <option value="x">Convex</option>
-                                <option value="f">Flat</option>
-                                <option value="k">Knobbed</option>
-                                <option value="s">Sunken</option>
+                                <option value="c">Cobwebby</option>
+                                <option value="e">Evanescent</option>
+                                <option value="f">Flaring</option>
+                                <option value="l">Large</option>
+                                <option value="n">None</option>
+                                <option value="p">Pendant</option>
+                                <option value="s">Sheathing</option>
+                                <option value="z">Zone</option>
                             </select>
                         </fieldset>
                     </div>
@@ -309,15 +297,15 @@
                     <fieldset class="form-group">
                         <label for="odor">Odor</label>
                         <select class="form-control" id="odor" name="odor">
-                            <option value="n">Brown</option>
-                            <option value="b">Buff</option>
-                            <option value="c">Cinnamon</option>
-                            <option value="g">Gray</option>
-                            <option value="p">Green</option>
-                            <option value="u">Pink</option>
-                            <option value="e">Purple</option>
-                            <option value="w">White</option>
-                            <option value="i">Yellow</option>
+                            <option value="a">Almond</option>
+                            <option value="l">Anise</option>
+                            <option value="c">Creosote</option>
+                            <option value="y">Fishy</option>
+                            <option value="f">Foul</option>
+                            <option value="m">Musty</option>
+                            <option value="n">None</option>
+                            <option value="p">Pungent</option>
+                            <option value="s">Spicy</option>
                         </select>
                     </fieldset>
                 </div>
@@ -326,12 +314,15 @@
                     <fieldset class="form-group">
                         <label for="sporePrintColor">Spore print color</label>
                         <select class="form-control" id="sporePrintColor" name="sporePrintColor">
-                            <option value="b">Bell</option>
-                            <option value="c">Conical</option>
-                            <option value="x">Convex</option>
-                            <option value="f">Flat</option>
-                            <option value="k">Knobbed</option>
-                            <option value="s">Sunken</option>
+                            <option value="n">Brown</option>
+                            <option value="b">Buff</option>
+                            <option value="k">Black</option>
+                            <option value="u">Purple</option>
+                            <option value="w">White</option>
+                            <option value="y">Yellow</option>
+                            <option value="h">Chocolate</option>
+                            <option value="o">Orange</option>
+                            <option value="r">Green</option>
                         </select>
                     </fieldset>
                 </div>
@@ -340,12 +331,12 @@
                     <fieldset class="form-group">
                         <label for="population">Population</label>
                         <select class="form-control" id="population" name="population">
-                            <option value="b">Bell</option>
-                            <option value="c">Conical</option>
-                            <option value="x">Convex</option>
-                            <option value="f">Flat</option>
-                            <option value="k">Knobbed</option>
-                            <option value="s">Sunken</option>
+                            <option value="a">Abundant</option>
+                            <option value="c">Clustered</option>
+                            <option value="n">Numerous</option>
+                            <option value="s">Scattered</option>
+                            <option value="v">Several</option>
+                            <option value="y">Solitary</option>
                         </select>
                     </fieldset>
                 </div>
@@ -353,12 +344,13 @@
                     <fieldset class="form-group">
                         <label for="habitat">Habitat</label>
                         <select class="form-control" id="habitat" name="habitat">
-                            <option value="b">Bell</option>
-                            <option value="c">Conical</option>
-                            <option value="x">Convex</option>
-                            <option value="f">Flat</option>
-                            <option value="k">Knobbed</option>
-                            <option value="s">Sunken</option>
+                            <option value="g">Grasses</option>
+                            <option value="l">Leaves</option>
+                            <option value="m">Meadows</option>
+                            <option value="p">Paths</option>
+                            <option value="u">Urban</option>
+                            <option value="w">Waste</option>
+                            <option value="d">Woods</option>
                         </select>
                     </fieldset>
                 </div>
