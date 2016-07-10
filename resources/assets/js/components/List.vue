5bfeaf6534d17957fbbcf8cf6873de28268ef6e4
Dario <dnedic@etfos.hr>
2016-07-10 20:53:57 +0200

    Updated List.vue with attributes

--- resources/assets/js/components/List.vue
+++ resources/assets/js/components/List.vue
@@ -27,73 +27,179 @@
             "s": "Sunken"
         },
         "capSurface": {
-
+            "f":"fibrous",
+            "g": "grooves",
+            "y": "scaly",
+            "s": "smooth"
         },
         "capColor": {
-
+            "n": "brown",
+            "b": "buff",
+            "c": "cinnamon",
+            "g": "gray",
+            "r": "green",
+            "p": "pink",
+            "u": "purple",
+            "e": "red",
+            "w": "white",
+            "y": "yellow"
         },
         "bruises": {
-
+            "t": "bruises",
+            "f": "no"   
         },
         "odor": {
-
+            "a": "almond",
+            "l": "anise",
+            "c": "creosote",
+            "y": "fishy",
+            "f": "foul",
+            "m": "musty",
+            "n": "none",
+            "p": "pungent",
+            "s": "spicy"
         },
         "gillAttachment": {
-
+            "a": "attached",
+            "d": "descending",
+            "f": "free",
+            "n": "notched"
         },
         "gillSpacing": {
-
+            "c": "close",
+            "w": "crowded",
+            "d": "distant"
+            
         },
         "gillSize": {
-
+            "b": "broad",
+            "n": "narrow"
         },
         "gillColor": {
-
+            "k": "black",
+            "n": "brown",
+            "b": "buff",
+            "h": "chocolate",
+            "g": "gray",
+            "r": "green",
+            "p": "pink",
+            "u": "purple",
+            "o": "orange",
+            "w": "white",
+            "y": "yellow",
+            "e": "red"
         },
         "stalkShape": {
+            "e": "enlarging",
+            "t": "tapering"
 
         },
         "stalkRoot": {
-
+            "b": "bulbous",
+            "c": "club",
+            "u": "cup",
+            "e": "equal",
+            "z": "rhizomorphs",
+            "r": "rooted",
+            "?": "missing" //upitnik zamijenit
         },
         "stalkSurfaceAboveRing": {
+            "f":"fibrous",
+            "k": "silky",
+            "y": "scaly",
+            "s": "smooth"
 
         },
         "stalkSurfaceBelowRing": {
-
+            "f":"fibrous",
+            "k": "silky",
+            "y": "scaly",
+            "s": "smooth"
         },
         "stalkColorAboveRing": {
+            "n": "brown",
+            "b": "buff",
+            "c": "cinnamon",
+            "g": "gray",
+            "o": "orange",
+            "p": "pink",
+            "e": "red",
+            "w": "white",
+            "y": "yellow"
 
         },
         "stalkColorBelowRing": {
-
+            "n": "brown",
+            "b": "buff",
+            "c": "cinnamon",
+            "g": "gray",
+            "o": "orange",
+            "p": "pink",
+            "e": "red",
+            "w": "white",
+            "y": "yellow"
         },
         "veilType": {
+            "p": "partial",
+            "u": "universal"
 
         },
         "veilColor": {
+            "n": "brown",
+            "o": "orange",
+            "w": "white",
+            "y": "yellow"
 
         },
         "ringNumber": {
-
+            "n": "none",
+            "o": "one",
+            "t": "two"
         },
         "ringType": {
-
+            "c": "cobwebby",
+            "e": "evanescent",
+            "f": "flaring",
+            "l": "large",
+            "n": "none",
+            "p": "pendant",
+            "s": "sheating",
+            "z": "zone",
         },
         "sporePrintColor": {
-
+            "k": "black",
+            "n": "brown",
+            "b": "buff",
+            "h": "chocolate",
+            "r": "green",
+            "o": "orange",
+            "u": "purple",
+            "w": "white",
+            "y": "yellow"
         },
         "population": {
-
+            "a": "abundant",
+            "c": "clustered",
+            "n": "numerous",
+            "s": "scattered",
+            "v": "several",
+            "y": "solitary"
         },
         "habitat": {
-
+            "g": "grasses",
+            "l": "leaves",
+            "m": "meadows",
+            "p": "paths",
+            "u": "urban",
+            "w": "waste",
+            "d": "woods"
         },
         "name": {
 
         },
         "result": {
-
+            "e": "edible",
+            "p": "poisonous"
         }
     }
 
